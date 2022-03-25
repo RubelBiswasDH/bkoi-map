51d6f4d408ab1c7c0413cadbed1572fde33058df
yeasin49 <myeasin49@gmail.com>
2022-03-25 19:38:25 +0600

    UI design style added

--- src/component/SearchPanel.jsx
+++ src/component/SearchPanel.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import * as dispatchFunction from '../redux-states/actionCreator';
 // import bkoigl from "bkoi-gl"
 import "../App.css";
+import {MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight}  from "react-icons/md";
 
 
 const SearchPanel = () => {
@@ -52,13 +53,22 @@ const SearchPanel = () => {
  
 
 return(
-    <div className="Search-panel-container"  style={mapData.themeType === 'dark' ? styleDark : styleLight}>
-       <button onClick={toggleThemehandler}>Change Theme</button>
-      <input className="bksearch" onChange={(e)=> setSearch(prevState => prevState = e.target.value)} placeholder="Search Location" value={search}/>
-      <div className="autocomplete-content">
+    <div className="search-panel-container"  style={mapData.themeType === 'dark' ? styleDark : styleLight}>
+      <div className="logo-container">
+        <a  href="/" className="logo">Bari<span className="logo-span">Koi</span></a>
+        <div className="toggle-search-panel">
+          <MdOutlineKeyboardArrowLeft />
+        </div>
+        {/* <div className="toggle-search-panel"><MdOutlineKeyboardArrowRight /></div> */}
+      </div>
+      <div className="input-container">
+        <button className="theme-changer" onClick={toggleThemehandler}>Change Theme</button>
+        <input className="bksearch" onChange={(e)=> setSearch(prevState => prevState = e.target.value)} placeholder="Search Location" value={search}/>
+      </div>
+      <div className="autocomplete-content" style={mapData.themeType === 'dark' ? styleDark : styleLight}>
           <ul className="scroll">
             {data && data.map((item,index) => (
-                <li key={item.id} onClick={() => selectedItemHandler(item)}>
+                <li className="search-option-list" key={item.id} onClick={() => selectedItemHandler(item)}>
                   <div className="location-icon">
                     <svg  aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map-marker-alt" className="svg-inline--fa fa-map-marker-alt fa-w-12 fa-lg location-svg " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" color="black"><path fill="currentColor" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path></svg>
                   </div>

480fd5e8f0062269a52663ad233b3d79b373beb1
yeasin49 <myeasin49@gmail.com>
2022-04-05 04:42:22 +0600

    zoom view control

--- src/component/MapPanel.jsx
+++ src/component/MapPanel.jsx
@@ -4,26 +4,50 @@ import React, { useEffect, useState } from "react"
 import bkoigl from "bkoi-gl"
 import "bkoi-gl/dist/style/bkoi-gl.css"
 import { useSelector } from "react-redux"
+import axios from "axios"
 
 const MapPanel = () => {
 
     const [mapMarker, setMapMarker] = useState();
     const [mapObject, setMapObject] = useState();
+    const [mapStyle, setMapStyle] = useState();
 
     const mapData = useSelector(prevState => prevState);
 
    
 
+    useEffect(()=>{
+        const fetchStyle = async () => {
+            try {
+                const response = await axios.get('https://map.barikoi.com/styles/osm-bright/style.json?key=MzE0MTowQzJLM0VQRTJV');
+                
+                response.data.layers[92].minzoom = 19;
+                response.data.layers[91].minzoom = 10;
+                // response.data.layers[14].paint['fill-color'] = "#F900BF"
+
+                setMapStyle(prevState => prevState = response.data);
+                console.log("response")
+                console.log(response.data.layers[91])
+
+            } catch (error) {
+                throw(error)
+            }
+        }
+        fetchStyle()
+    },[])
+
+
     // creating map and navigation control 
     useEffect(()=> {
-       
+       console.log("entered")
         bkoigl.accessToken = process.env.REACT_APP_BKOI_API // required
          const map = new bkoigl.Map({
             container: 'map',  
             center: mapData.currentLocation ? [  mapData.currentLocation.long,  mapData.currentLocation.lat ] : [ 90.3938010872331, 23.821600277500405 ],  
             zoom: 13,
             // controlPositions: 'topLeft',
-            style: mapData.themeType === 'dark' ? 'https://map.barikoi.com/styles/barikoi-dark/style.json' :  ''
+            style: mapData.themeType === 'dark' ? 'https://map.barikoi.com/styles/barikoi-dark/style.json' :  mapStyle
+            // style: mapStyle
         });
 
         // Navigation Control Loader
@@ -51,7 +75,7 @@ const MapPanel = () => {
         window.removeEventListener("load", controlLoader)
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[mapData.themeType])
+    },[mapData.themeType, mapStyle])
  
    
 
@@ -91,6 +115,25 @@ const MapPanel = () => {
         else if(property === "shop") return "pink";
         else return "green"
     }
+
+    // handlers
+    mapObject && mapObject.on('style.load', function() {
+
+        mapObject.on('click', function(e) {
+        let l = mapObject.getStyle().layers;
+
+         
+            var features = mapObject.queryRenderedFeatures(e.point);
+            console.log({ features })
+        
+      
+            new bkoigl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML('you clicked here: <br/>' + features[0].properties.ucode)
+                .addTo(mapObject);
+                console.log(features[0].properties)
+        });
+      });
 
   
 return(

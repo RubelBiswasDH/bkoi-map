688722da39a9e07c32fbc7b4679ecf69086c882a
RubelBiswasDH <RubelBiswasDH@gmail.com>
2022-04-17 14:19:42 +0600

    added new object in layer list

--- src/component/MapPanel.jsx
+++ src/component/MapPanel.jsx
@@ -18,12 +18,52 @@ const MapPanel = () => {
 
     useEffect(()=>{
         const fetchStyle = async () => {
+            const bkoi_data = {
+                id: "bkoi",
+                type: "symbol",
+                source: "data",
+                "source-layer": "ucode",
+                minzoom: 5,
+                filter: ["all", ["==", "$type", "Point"], ["in", "ucode", "BKOI2017"]],
+                layout: {
+                    "icon-image": "office_11",
+                    "text-anchor": "left",
+                    "text-field": "{name_en}",
+                    "text-font": ["Roboto Condensed Italic"],
+                    "text-max-width" : 9,
+                    "text-offset" : [0.8,0.2],
+                    "text-size" : 16,
+                    "visibility":"visible",
+                },
+                paint:{
+                    "text-color" : "red",
+                    "text-halo-blur" : 0.5,
+                    "text-halo-color" : "#ffffff",
+                    "text-halo-width" : 1
+                }
+            }
+
+
             try {
                 const response = await axios.get('https://map.barikoi.com/styles/osm-bright/style.json?key='+`${accessToken}`);
                 
                 response.data.layers[92].minzoom = 19;
                 response.data.layers[91].minzoom = 10;
-                response.data.layers[14].paint['fill-color'] = "green"
+                response.data.layers[14].paint['fill-color'] = "gray";
+
+                response.data.layers[2].paint['fill-color'] = "red";
+                response.data.layers[38].paint['fill-color'] = "orangered";
+                response.data.layers[45].paint['line-color'] = "black";
+                response.data.layers[45].paint['line-color'] = "black";
+                response.data.layers[96].paint['text-color'] = "red";
+                response.data.layers[39].paint['fill-extrusion-color'] = "yellow";
+                response.data.layers[39].minzoom= 18;
+                response.data.layers[12].paint['line-color'] = "blue";
+                response.data.layers[13].paint['line-color'] = "blue";
+                response.data.layers[58].paint['line-color'] = "red";
+                response.data.layers.push(bkoi_data)
+
+                //response.data.layers[96].paint['text-halo-color'] = "red";
                 //console.log(response.data.layers.slice(0,5))
                 setMapStyle(prevState => prevState = response.data);
                 console.log("response")
